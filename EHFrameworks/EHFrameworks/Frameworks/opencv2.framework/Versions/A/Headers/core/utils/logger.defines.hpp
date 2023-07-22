8931e3a9b699800abe71a048c564f518c0a390b8
黄坤鹏 <leo.huang@easyhealthcarecorp.com>
2023-07-22 17:50:16 +0800

    1

--- EHFrameworks/EHFrameworks/Frameworks/opencv2.framework/Versions/A/Headers/core/utils/logger.defines.hpp
+++ EHFrameworks/EHFrameworks/Frameworks/opencv2.framework/Versions/A/Headers/core/utils/logger.defines.hpp
@@ -1,22 +1,3 @@
-// This file is part of OpenCV project.
-// It is subject to the license terms in the LICENSE file found in the top-level directory
-// of this distribution and at http://opencv.org/license.html.
-
-#ifndef OPENCV_LOGGER_DEFINES_HPP
-#define OPENCV_LOGGER_DEFINES_HPP
-
-//! @addtogroup core_logging
-//! @{
-
-// Supported logging levels and their semantic
-#define CV_LOG_LEVEL_SILENT 0          //!< for using in setLogLevel() call
-#define CV_LOG_LEVEL_FATAL 1           //!< Fatal (critical) error (unrecoverable internal error)
-#define CV_LOG_LEVEL_ERROR 2           //!< Error message
-#define CV_LOG_LEVEL_WARN 3            //!< Warning message
-#define CV_LOG_LEVEL_INFO 4            //!< Info message
-#define CV_LOG_LEVEL_DEBUG 5           //!< Debug message. Disabled in the "Release" build.
-#define CV_LOG_LEVEL_VERBOSE 6         //!< Verbose (trace) messages. Requires verbosity level. Disabled in the "Release" build.
-
-//! @}
-
-#endif // OPENCV_LOGGER_DEFINES_HPP
+version https://git-lfs.github.com/spec/v1
+oid sha256:42423219ba6a4258d9c6d37b1c21c3b669cba676747638cb2ff818877242b733
+size 963
